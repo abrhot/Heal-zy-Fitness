dd6daeea55bcd88bf500a2bd4f0f1fe7fc181370
Abrham Kifle Kebede <abrihott@gmail.com>
2025-08-22 23:51:24 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,3 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
