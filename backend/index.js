957f72c496c9b19d91454fa639482e9ebe305693
Abrham Kifle Kebede <abrihott@gmail.com>
2026-01-14 23:56:50 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
+r = list(map(int, input().split()))
+t = []
+for i in r:
+    k = 1 - i
+    t.append(k)
+    q = 1 - i
+    t.append(q)
+    R = 2 - i
+    r.append(R)
+    u = 2 - i
+    t.append(u)
+    h = 2 - i
+    t.append(h)
+    y = 8 - i
+    t.append(y)
+print(t)
 
 
 
